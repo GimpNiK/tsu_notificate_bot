f4f11e476d4d72514b812a1e8028d9e12a78d96f
GimpNiK <131478999+GimpNiK@users.noreply.github.com>
2025-12-23 14:16:02 +0300

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -1,25 +1,41 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
 
 import asyncio
 import datetime
 import logging
 
-from data import  get_schedule,chat_id_load,chat_id_save,get_time_lesson
-from config import tg_token,teacher,send_time,delta_days
+from data import  get_schedule,chat_id_load,chat_id_save
+from config import (
+                    BOT_TOKEN,
+                    TEACHER,
+                    SEND_TIME,
+                    DELTA_DAYS,
+                    
+                    START_MESSAGE,
+                    MAIN_MESSAGE_HEAD,
+                    MAIN_MESSAGE_BODY,
+                    MAIN_MESSAGE_END,
+
+                    BOT_LOG,
+                    TIME_FORMAT,
+                    RATE_CHECK_DATETIME,
+                    MAX_ERROR,
+                    ENCODING
+)
+
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('bot.log', encoding='utf-8'),
+        logging.FileHandler(BOT_LOG, encoding = ENCODING),
         logging.StreamHandler()
     ]
 )
 
 logger = logging.getLogger(__name__)
 
-bot = Bot(token = tg_token)
+bot = Bot(token = BOT_TOKEN)
 dp = Dispatcher()
 chat_id = chat_id_load()
 
@@ -32,22 +48,22 @@ async def start(event: types.ChatMemberUpdated):
     chat_id_save(chat_id)
     
 
-    logger.info(f"Новый старт: Chat ID={chat_id}, User ID={event.from_user.id}")  # type: ignore
-    await event.answer("✅ Чат зарегистрирован для получения уведомлений!")
+    logger.info(f"Start: Chat ID={chat_id}, User ID={event.from_user.id}")
+    await event.answer(START_MESSAGE)
 
 async def send_notification():
 
-    date = (datetime.date.today() + datetime.timedelta(days=delta_days)).strftime("%d.%m.%Y")
+    date = (datetime.date.today() + datetime.timedelta(days=DELTA_DAYS)).strftime("%d.%m.%Y")
     try:
-        schedule_data = get_schedule(teacher, date)
+        schedule_data = get_schedule(TEACHER, date)
     except Exception as e:
-        logger.error(f"Проблема с получением расписания {e}")
+        logger.error(f"Problem with getting the schedule {e}")
     if not schedule_data or chat_id is None:
         return
 
 
     groups_usable = set()
-    message = f"📅 Расписание на {date}:\n"
+    message = MAIN_MESSAGE_HEAD(date)
     for pair in schedule_data:
         groups_uniq = []
         for group in schedule_data[pair]:
@@ -55,9 +71,9 @@ async def send_notification():
                 groups_usable.add(group)
                 groups_uniq.append(group)
         if groups_uniq:
-            message += f"В {get_time_lesson[pair]} у групп:"  + ", ".join(groups_uniq) + "\n"
+            message += MAIN_MESSAGE_BODY(pair, groups_uniq)
     
-    message += f"Завтра занятие. Не опаздывать!"
+    message += MAIN_MESSAGE_END
     
     await bot.send_message( chat_id=chat_id, text=message)
 
@@ -65,9 +81,9 @@ async def send_notification():
 
 async def send_message_by_schedule():
     while True:
-        if datetime.datetime.now().strftime('%H:%M')  == send_time:
+        if datetime.datetime.now().strftime(TIME_FORMAT)  == SEND_TIME:
             await send_notification()
-        await asyncio.sleep(60)
+        await asyncio.sleep(RATE_CHECK_DATETIME)
 
 
 async def main():
@@ -76,9 +92,9 @@ async def main():
     await scheduler_task
 
 if __name__ == "__main__":
-    for i in range(100):
+    for i in range(MAX_ERROR):
         try:
             asyncio.run(main())
         except Exception as e:
-            logger.error(f"Критическая ошибка: {e}")
-    logger.error("Лимит критических ошибок переполнен. Пожалуйста перезапустите приложение.")
+            logger.error(f"Critical error: {e}")
+    logger.error("The critical error limit is full. Please restart the application.")
